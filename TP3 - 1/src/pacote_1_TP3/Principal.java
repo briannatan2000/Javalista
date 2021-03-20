b1ca0669adab09bda84e15f102bd73eb1025898b
Brian Natã <Brian Nat@192.168.100.102>
2021-03-20 18:08:26 -0300

    Primeiro commit

---
+++ TP3 - 1/src/pacote_1_TP3/Principal.java
@@ -0,0 +1,12 @@
+package pacote_1_TP3;
+
+public class Principal {
+	public static void main(String[] args) {
+		Produto p1 = new Produto();
+		p1.nome = "Abacaxi";
+		p1.quantidade = 2;
+		p1.unidade = "unidade";
+		p1.preco = 4.5;
+		p1.categoria = "Frutas";
+	}
+}
---
+++ TP3 - 1/src/pacote_1_TP3/Produto.java
@@ -0,0 +1,22 @@
+package pacote_1_TP3;
+
+public class Produto {
+//Atributos
+	String nome;
+	int quantidade;
+	String unidade;
+	double preco;
+	String categoria;
+	
+//Metodos
+	void cadastrar() {
+		
+	}
+	
+	void alterar() {
+		
+	}
+	void excluir(){
+		
+	}
+}
