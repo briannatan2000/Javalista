34e9425fc59312c6d09f072d9906f7c6cb244c60
Brian Natã <Brian Nat@192.168.100.102>
2021-03-21 18:21:01 -0300

    Atualização das classes "Produto" e "Principal"

--- TP3 - 1/src/pacote_1_TP3/Principal.java
+++ TP3 - 1/src/pacote_1_TP3/Principal.java
@@ -1,12 +1,12 @@
 package pacote_1_TP3;
 
 public class Principal {
+	
+	static Produto p1;
+	
 	public static void main(String[] args) {
-		Produto p1 = new Produto();
-		p1.nome = "Abacaxi";
-		p1.quantidade = 2;
-		p1.unidade = "unidade";
-		p1.preco = 4.5;
-		p1.categoria = "Frutas";
+		p1 = new Produto("Biscoito", 4, "un", 8.96, "Lanches");
+		
+		System.out.println(p1.toString());
 	}
 }
--- TP3 - 1/src/pacote_1_TP3/Produto.java
+++ TP3 - 1/src/pacote_1_TP3/Produto.java
@@ -2,23 +2,58 @@ package pacote_1_TP3;
 
 public class Produto {
 //Atributos
-	String nome;
-	int quantidade;
-	String unidade;
-	double preco;
-	String categoria;
-	
+	private String nome;
+	private int quantidade;
+	private String unidade;
+	private double preco;
+	private String categoria;
 	
 	
 //Metodos (to pensando em colocar os métodos como comprar do tipo boolean e cadastrar, alterar e excluir nas listas)
-	void cadastrar() {
-		
+	
+	public Produto(String a, int b, String c, double d, String e){
+		nome = a;
+		quantidade = b;
+		unidade = c;
+		preco = d;
+		categoria = e;
 	}
 	
-	void alterar() {
-		
+	public String getNome() {
+		return nome;
 	}
-	void excluir(){
-		
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getUnidade() {
+		return unidade;
+	}
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome do Produto=" + nome + ", quantidade=" + quantidade + ", unidade=" + unidade + ", preco=" + preco
+				+ ", categoria=" + categoria;
+	}
+
+	}
